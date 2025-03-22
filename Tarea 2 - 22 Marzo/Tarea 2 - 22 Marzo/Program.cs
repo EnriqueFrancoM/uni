5c9ddedc29d08206bf688d0771ae1986c73c47a4
EnriqueFrancoM <enriquecba10@gmail.com>
2025-03-22 14:44:30 -0400

    Add files via upload
    Tarea 2 - 22 Marzo
    Linea 123 del codigo, modificar las coordenadas del nuevo eje central (x,y,z) para mover la figura, sin tocar los vertices.

---
+++ Tarea 2 - 22 Marzo/Tarea 2 - 22 Marzo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTK_U
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new OpenTK.Mathematics.Vector2i(800, 600),
+                Title = "Letra U 3D - Tarea 2 - 22 Marzo",
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
